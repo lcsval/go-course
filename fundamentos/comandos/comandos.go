2fcf1ea777d081208f8c0cc67093fa0ea4eb4c35
Lucas Val <lucas_val@outlook.com>
2020-09-22 16:51:11 -0300

    fundamentos

--- fundamentos/comandos/comandos.go
+++ fundamentos/comandos/comandos.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("teste!")
+	fmt.Printf("Outro programa em %s", "Go")
 }
---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,15 @@
+// Programas executáveis inciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
